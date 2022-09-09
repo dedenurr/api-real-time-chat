020b81244c0bb2f4f039e3a7e8a9b61d3835fe19
dedenurr <dedenurrahman.git@gmail.com>
2022-09-10 05:28:50 +0700

    [CHG] update connection DB

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-dotenv.config;
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+  },
+  () => {
+    console.log('Connected to mongodb Succes');
+  }
+);
 
 app.listen(8800, () => {
   console.log('Backend server, Api chat realtime is Running');

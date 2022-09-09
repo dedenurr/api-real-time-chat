4178f2cc0d0bd14f984d83c010f8842e50f5c2a3
dedenurr <dedenurrahman.git@gmail.com>
2022-09-10 05:47:05 +0700

    [CHG] update middleware

--- index.js
+++ index.js
@@ -7,19 +7,23 @@ const morgan = require('morgan');
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-  },
-  () => {
-    console.log('Connected to mongodb Succes');
-  }
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('Connected to mongodb Succes');
+});
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get('/', (req, res, next) => {
+  res.send('welcome to api chat realtime');
+});
+
+app.get('/users', (req, res, next) => {
+  res.send('welcome to users chat realtime');
+});
 
 app.listen(8800, () => {
-  console.log('Backend server, Api chat realtime is Running');
+  console.log('Backend server, Api chat realtime is Running ');
 });

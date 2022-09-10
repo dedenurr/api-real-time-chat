52b7a41ab88dc6bbc0fd35212961534f3661366f
dedenurr <dedenurrahman.git@gmail.com>
2022-09-11 05:29:52 +0700

    [ADD] Auth route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -17,8 +18,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
+// Api
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server, Api chat realtime is Running ');
 });
+
+//https://youtu.be/ldGl6L4Vktk?list=PLj-4DlPRT48lXaz5YLvbLC38m25W9Kmqy&t=793

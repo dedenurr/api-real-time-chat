aaa266783b2b75e8ec9a5cd7ba145fce0e6ae2d3
dedenurr <dedenurrahman.git@gmail.com>
2022-09-11 09:22:13 +0700

    [ADD] route get Conversation

--- routes/conversation.js
+++ routes/conversation.js
@@ -16,5 +16,14 @@ router.post('/', async (req, res, next) => {
 });
 
 // get conversation of a user
-
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;

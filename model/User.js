96075a3c03ee9b3b3ef05619c96769bc1aa46dab
dedenurr <dedenurrahman.git@gmail.com>
2022-09-11 19:07:40 +0700

    [CHG] User Model & chat-api.postman_collection

--- model/User.js
+++ model/User.js
@@ -20,29 +20,9 @@ const UserSchema = new mongoose.Schema(
       required: true,
       min: 6,
     },
-    profilePicture: {
-      type: String,
-      default: '',
-    },
-    coverPicture: {
-      type: String,
-      default: '',
-    },
-    followers: {
-      type: Array,
-      default: [],
-    },
-    followings: {
-      type: Array,
-      default: [],
-    },
     isAdmin: {
       type: Boolean,
       default: false,
-    },
-    desc: {
-      type: String,
-      max: 50,
     },
     city: {
       type: String,
@@ -51,10 +31,6 @@ const UserSchema = new mongoose.Schema(
     from: {
       type: String,
       max: 50,
-    },
-    relationship: {
-      type: Number,
-      enum: [1, 2, 3],
     },
   },
   { timestamps: true }
